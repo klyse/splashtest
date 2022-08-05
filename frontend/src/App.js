ee226bf5797bea75f53c7b3dffcda236d7641808
icate95 <ianesellicaterina@gmail.com>
2022-08-05 20:16:27 +0200

    new header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link as NavLink } from 'react-router-dom';
 import Create from './Create';
 import List from './List';
 import {
@@ -8,22 +8,35 @@ import {
   Container,
   VStack,
   Code,
-  Grid,
+  ButtonGroup,
+  Spacer,
   theme,
+  Image,
+  Button,
+  Flex,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import logo from '../src/splash_test.png';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Container maxW="container.xl">
         <Box textAlign="center" fontSize="xl">
-          <h1>Welcome to Splashtest!</h1>
-
-          <Link to="/create">Create new test</Link>
-          <Link to="/list">Check all the tests</Link>
-
+          <Flex minWidth="max-content" alignItems="center" gap="2">
+            <Box p="2">
+              <Image w={200} src={logo} alt="Dan Abramov" />
+            </Box>
+            <Spacer />
+            <ButtonGroup gap="2">
+              <Button m={[0, 3]} color="#3e94ff" as={NavLink} to="/create">
+                Create new test
+              </Button>
+              <Button m={[0, 3]} color="#3e94ff" as={NavLink} to="/list">
+                Check all the tests
+              </Button>
+            </ButtonGroup>
+          </Flex>
           <Box p={5}>
             <Routes>
               <Route path="create" element={<Create />} />
--- frontend/src/List/index.js
+++ frontend/src/List/index.js
@@ -1,5 +1,50 @@
+import {
+  Box,
+  Flex,
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
+  Button,
+} from '@chakra-ui/react';
+
 function List() {
-  return <div>here the list</div>;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const handleClick = () => {
+    onOpen();
+  };
+
+  return (
+    <Flex spacing={8} direction="row" gap="8">
+      <Box p={5} bg="#fafafa" flexGrow="1" shadow="md" borderWidth="1px">
+        <Button onClick={() => handleClick()} m={4}>{`Open Drawer`}</Button>
+
+        <Drawer onClose={onClose} isOpen={isOpen} size="xl">
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerHeader> drawer contents</DrawerHeader>
+            <DrawerBody>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Consequat nisl vel pretium lectus quam id. Semper quis lectus
+                nulla at volutpat diam ut venenatis. Dolor morbi non arcu risus
+                quis varius quam quisque. Massa ultricies mi quis hendrerit
+                dolor magna eget est lorem. Erat imperdiet sed euismod nisi
+                porta. Lectus vestibulum mattis ullamcorper velit.
+              </p>
+            </DrawerBody>
+          </DrawerContent>
+        </Drawer>
+      </Box>
+    </Flex>
+  );
 }
 
 export default List;
