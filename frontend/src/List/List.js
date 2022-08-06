0fbf6ca247e8a523a4957da746b6d426bdaad843
icate95 <ianesellicaterina@gmail.com>
2022-08-06 09:14:14 +0200

    new row runs table

--- frontend/src/List/List.js
+++ frontend/src/List/List.js
@@ -28,41 +28,78 @@ import {
 import { useToast } from '@chakra-ui/react';
 import { useState } from 'react';
 
-function MySelector() {
+function Infos(props) {
   const [schedule, setSchedule] = useState('');
-  return (
-    <FormControl>
-      <Select
-        placeholder=""
-        value={schedule}
-        onChange={e => setSchedule(e.target.value)}
-      >
-        <option value=""></option>
-        <option value="option1">every 30 min</option>
-        <option value="option2">every 6 hours</option>
-        <option value="option3">every day</option>
-      </Select>
-    </FormControl>
-  );
-}
-
-function MoreInfo(props) {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   const handleClick = () => {
     onOpen();
   };
+  const toast = useToast();
+  const run = async id => {
+    try {
+      const response = await fetch(`http://localhost:5001/test/${id}/run`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+      });
+      toast({
+        title: 'Test created.',
+        description: `New run executed`,
+        status: 'info',
+        duration: 9000,
+        isClosable: true,
+      });
+    } catch (e) {
+      toast({
+        title: 'Error.',
+        description: `Cannot create run`,
+        status: 'error',
+        duration: 9000,
+        isClosable: true,
+      });
+    }
+  };
 
   return (
-    <>
-      <Button
-        onClick={() => handleClick()}
-        m={4}
-        disabled={props.item.runs.length === 0}
-      >
-        View runs
-      </Button>
+    <Tr>
+      <Td>{props.item.name}</Td>
+      <Td>
+        <Button
+          onClick={() => {
+            run(props.item.id);
+          }}
+          m={4}
+        >
+          Run test
+        </Button>
+      </Td>
 
+      <Td>
+        <FormControl>
+          <Select
+            placeholder=""
+            value={schedule}
+            onChange={e => setSchedule(e.target.value)}
+          >
+            <option value=""></option>
+            <option value="option1">every 30 min</option>
+            <option value="option2">every 6 hours</option>
+            <option value="option3">every day</option>
+          </Select>
+        </FormControl>
+      </Td>
+      <Td>
+        <Button
+          onClick={() => handleClick()}
+          m={4}
+          disabled={props.item.runs.length === 0}
+        >
+          View runs
+        </Button>
+      </Td>
       <Drawer onClose={onClose} isOpen={isOpen} size="xl">
         <DrawerOverlay />
         <DrawerContent>
@@ -103,44 +140,11 @@ function MoreInfo(props) {
           </DrawerBody>
         </DrawerContent>
       </Drawer>
-    </>
+    </Tr>
   );
 }
 
 function MyList(props) {
-  const toast = useToast();
-  const run = async id => {
-    try {
-      const response = await fetch(`http://localhost:5001/test/${id}/run`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json',
-        },
-      });
-      toast({
-        title: 'Test created.',
-        description: `New run executed`,
-        status: 'info',
-        duration: 9000,
-        isClosable: true,
-      });
-      // setTitle('');
-      // setAuthor('');
-      // setEmail('');
-      // setInterval('');
-      // // setSchedule('');
-      // setItem([]);
-    } catch (e) {
-      toast({
-        title: 'Error.',
-        description: `Cannot create run`,
-        status: 'error',
-        duration: 9000,
-        isClosable: true,
-      });
-    }
-  };
   return (
     <TableContainer>
       <Table variant="simple">
@@ -154,30 +158,11 @@ function MyList(props) {
         </Thead>
         <Tbody>
           {props?.items?.map(item => (
-            <Tr>
-              <Td>{item.name}</Td>
-              <Td>
-                <Button
-                  onClick={() => {
-                    run(item.id);
-                  }}
-                  m={4}
-                >
-                  Run test
-                </Button>
-              </Td>
-              <Td>
-                <MySelector />
-              </Td>
-              <Td>
-                <MoreInfo item={item} />
-              </Td>
-            </Tr>
+            <Infos item={item} />
           ))}
         </Tbody>
       </Table>
     </TableContainer>
   );
 }
-
 export default MyList;
