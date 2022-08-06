d5e7567524dd4ff440dd280b6349b1ed5d121c06
klyse <klaus.pruenster@gmail.com>
2022-08-06 07:45:13 +0200

    added date time sorting for tests

--- backend/web/Models/TestDto.cs
+++ backend/web/Models/TestDto.cs
@@ -4,6 +4,7 @@ public class TestDto
 {
     public Guid Id { get; set; }
     public string Name { get; set; }
+    public DateTime CreatedDateTime { get; set; }
     public ICollection<TestStep>? TestCode { get; set; }
     public ICollection<RunDto> Runs { get; set; }
 }
